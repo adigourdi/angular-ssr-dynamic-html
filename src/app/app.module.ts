9c0721baac05453acd65b0da2db7e37dfcaaff66
Abderrahim Digourdi <a.digourdi@gmail.com>
2021-11-08 18:23:36 +0100

    feat: add experimental ssr clover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RendererModule, TransferHttpCacheModule } from '@nguniversal/common/clover';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'appId' }), RendererModule.forRoot(), TransferHttpCacheModule],
   providers: [],
   bootstrap: [AppComponent],
 })
